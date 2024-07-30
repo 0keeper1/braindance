2164899acb174cb2a105d1761f959fd18a21e0c4
The Keeper <abysskeeper@protonmail.com>
2024-07-30 23:40:38 +0330

    test folder Added.

---
+++ test/buffertests/linestest.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "nanotest/src/lib.h"
+#include "src/core/buffer/lines.h"
+
+void test_linesCreate();
+
+extern void test_lines();
---
+++ test/maintest.c
@@ -0,0 +1,9 @@
+#include "./buffertests/linestest.h"
+#include "nanotest/src/lib.h"
+#include <stdio.h>
+
+int main( void )
+{
+	NANO_SINGLE_TEST( test_lines );
+	return 0;
+}
