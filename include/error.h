7515fdcf6193d79ce2efee93d2e2023e6c291738
0xkeeper <abysskeeper@protonmail.com>
2024-12-12 02:33:53 +0330

    feat(error): add new errors

--- include/error.h
+++ include/error.h
@@ -16,6 +16,7 @@ typedef enum ErrorCode : char
 	// std errors
 	STD_ERR_COPY_STRING_FAILED,
 	STD_ERR_REALLOCATION_FAILED,
+	STD_ERR_REQUEST_FOR_MEMORY_FAILED,
 
 	// command line errors
 	CLI_ERR_INVALID_COMMAND,
@@ -28,9 +29,14 @@ typedef enum ErrorCode : char
 	FILE_ERR_OPEN_FAILED,
 	FILE_ERR_PARSE_CTX_FAILED,
 	FILE_ERR_FILE_NOT_FOUND,
+	FILE_ERR_SEEK_FAILED,
 
 	// path errors
 	PATH_ERR_INVALID_PATH,
+
+	// Char errors
+	CHAR_ERR_ONLY_ESC_ALLOWED,
+	CHAR_ERR_ESC_NOT_ALLOWED,
 } ErrorCode;
 
 /**
